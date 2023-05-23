1e1cb3947bced0495bce06364b2ac6c79b0c0e22
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-05-23 14:12:57 +0100

    Fix linters

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :recipe do
-    name { "MyString" }
-    preparation_time { "MyString" }
-    cooking_time { "MyString" }
-    description { "MyText" }
+    name { 'my_string' }
+    preparation_time { 'my_string' }
+    cooking_time { 'my_string' }
+    description { 'my+text' }
     public { false }
     user { nil }
   end
