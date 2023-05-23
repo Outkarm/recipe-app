3af84a4bc450c0a4731e50919d3a23a7a9fba2a7
Outkarm <jkpordje@gmail.com>
2023-05-23 23:45:11 +0000

    update user modle with devise methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  #  :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :confirmable, :rememberable, :validatable
   validates :name, presence: true
   has_many :foods
 end
