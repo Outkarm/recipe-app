1ed5afebc7ecc9734333889fd9a629cdca383a90
Outkarm <jkpordje@gmail.com>
2023-05-23 23:42:43 +0000

    configure foods controller with user athentication

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,14 +1,25 @@
 class FoodsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @food = Food.all
   end
 
   def show
+    return unless user_signed_in?
+
     @food = Food.find(params[:user_id])
   end
 
   def create
+    return unless user_signed_in?
+
     @food = Food.new(food_params)
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path, notice: 'Food item deleted successfully.'
   end
 
   private
