7330f69af85da23e366997a0609e40414a4c3692
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-05-24 19:25:58 +0100

    Add styles to public recipe index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,7 @@ class RecipesController < ApplicationController
   end
 
   def show
-    @recipe = Recipe.includes(:recipe_food).find_by(id: params[:id])
+    @recipe = Recipe.includes(:recipe_foods).find_by(id: params[:id])
   end
 
   def new; end
@@ -13,9 +13,14 @@ class RecipesController < ApplicationController
     @recipe = Recipe.create(recipe_params)
   end
 
-  def destory
-    @recipe = Recipe.find_by(id: params[:id])
-    @recipe.destroy
+  def destroy
+    recipe = Recipe.find(params[:id])
+    if recipe.destroy
+      flash[:success] = 'Recipe deleted successfully'
+      redirect_to recipes_path
+    else
+      flash.now[:error] = "Recipe couldn't be deleted"
+    end
   end
 
   private
