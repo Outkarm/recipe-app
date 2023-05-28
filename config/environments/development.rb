9c026ea126da839e2f237c37be30156a8ef45e6e
Outkarm <jkpordje@gmail.com>
2023-05-28 22:37:48 +0000

    configure the mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,5 +71,5 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'recipe-tnhd.onrender.com'}
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
@@ -90,4 +90,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    user_name: "ourrecipea@gmail.com",
+    password: "Recipe??3567@app",
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 end
